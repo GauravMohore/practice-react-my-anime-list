43c3993fd68997c6866ef9ee7849d1b0413afafd
GauravMohore <75619409+GauravMohore@users.noreply.github.com>
2023-03-09 23:13:29 +0530

    second commit

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,21 +1,21 @@
 import Card from "./Card";
-import SetData from "./SetData"
-
-function setCard(val) {
-    return (
-      <Card
-        img={val.img}
-        title={val.title}
-        name={val.name}
-        link={val.links}
-      />
-    );
-}
+import SetData from "./SetData";
 
 export default function CardContainer() {
   return (
+    <>
       <div className="card-container">
-          {SetData.map(setCard)}
-    </div>
+        {SetData.map((val) => {
+          return (
+            <Card
+              img={val.img}
+              title={val.title}
+              name={val.name}
+              link={val.links}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
