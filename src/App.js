d857501056db9f0991f52a2f77a9517d8d2b3b64
GauravMohore <75619409+GauravMohore@users.noreply.github.com>
2023-03-11 17:23:03 +0530

    please stop added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import CardContainer from "./components/CardContainer";
 import Heading from "./components/Heading";
+import SmallCase from "./components/SmallCase";
 import "./index.css"
 
 function App() {
   return (
     <>
       <Heading />
-      <CardContainer/>
+      <CardContainer />
+      <SmallCase/>
     </>
   );
 }
---
+++ src/components/SmallCase.jsx
@@ -0,0 +1,11 @@
+import "./components.css";
+
+export default function SmallCase() {
+  return (
+    <div className="small-case">
+      <h1>
+        やめて <span className="sc-span">ください</span>
+      </h1>
+    </div>
+  );
+}
